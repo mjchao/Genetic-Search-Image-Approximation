c7b82a34104cf9c805ddcc7ea4b53052ae1c3601
Mickey Chao <mjchao2@gmail.com>
2015-09-25 21:17:02 -0400

    Upload code to generate fitness plots and image of Turing

---
+++ Plot.py
@@ -0,0 +1,32 @@
+'''
+Created on Sep 25, 2015
+
+@author: mjchao
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+class Plot( object ):
+    
+    @staticmethod
+    def plot_data( fitness_stats ):
+        f = open( fitness_stats , 'r' )
+        x = []
+        y = []
+        for line in f.readlines():
+            data = re.findall( r'\d*\.?\d+' , line )
+            print str( data )
+            if ( len( data ) >= 2 ):
+                generation = data[ 0 ]
+                fitness = data[ 1 ]
+                x.append( generation )
+                y.append( fitness )
+         
+        x = np.array( x )
+        y = np.array( y )
+        plt.plot( x , y )
+        plt.show()
+         
+Plot.plot_data( "mona_lisa2.out" )   
+        
