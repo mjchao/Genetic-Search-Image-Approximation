e658d6075198c3822afc379fda443fd5a8797903
Mickey Chao <mjchao2@gmail.com>
2015-09-27 15:12:45 -0400

    Remove debug code

--- Search.py
+++ Search.py
@@ -44,14 +44,6 @@ class Search( object ):
     @staticmethod
     def save_code_as_image( code , gen ):
         filename = OUTPUT_DIR + "/" + str( gen ) + ".png"
-        #surface = pygame.surface.Surface( (IMG_WIDTH, IMG_HEIGHT) , flags = pygame.SRCALPHA )
-        Search.window.fill( (0,0,0) )
-        code.draw_onto_screen( Search.window )
-        GeneticCode.surface.fill( (0,0,0) )
-        code.draw_onto_surface( GeneticCode.surface )
-        pixelArray = convertToPixelArray( GeneticCode.surface )
-        eucDist = euclideanDistance( pixelArray , IMG_PIXEL_ARRAY )
-        print eucDist 
         
         #draw the generation number
         label = Search.generationFont.render( str( gen ) , 1 , (255,255,255) )
